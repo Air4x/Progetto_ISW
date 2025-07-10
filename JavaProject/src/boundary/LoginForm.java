e601dcb8f7337254b8b49166d13e9546b2610391
FraCalk <francescocalk11@gmail.com>
2025-07-10 12:39:49 +0200

    Modifiche al package boundary

---
+++ JavaProject/src/boundary/LoginForm.java
@@ -0,0 +1,76 @@
+package boundary;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class LoginForm  extends JFrame {
+    private JTextField emailTextField;
+    private JLabel lblemail;
+    private JPasswordField passwordfield;
+    private JLabel lblpassword;
+    private JPanel panel;
+    private JButton sendLoginButton;
+
+    public LoginForm() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(200,300);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        JPanel panel = new JPanel();
+        panel.setBounds(5,5,5,5);
+        setTitle("Login Form");
+
+        panel.setLayout(null);
+        setContentPane(panel);
+
+        JLabel lblEmail = new JLabel("Email:");
+        lblemail.setBounds(10,10,100,30 );
+        lblemail.setFont(new Font("Arial",Font.PLAIN,20));
+        panel.add(lblemail);
+
+
+        emailTextField = new JTextField();
+        emailTextField.setBounds(10,50,150,30);
+        panel.add(emailTextField);
+
+        JLabel lblpassword = new JLabel("Password:");
+        lblpassword.setFont(new Font("Arial",Font.PLAIN,20));
+        lblpassword.setBounds(10,90,100,30 );
+        panel.add(lblpassword);
+
+        passwordfield = new JPasswordField();
+        passwordfield.setBounds(10,130,150,30);
+        panel.add(passwordfield);
+
+        JButton sendLoginButton = new JButton("Send Login");
+        sendLoginButton.setBounds(10,170,100,30);
+        sendLoginButton.addMouseListener(new MouseAdapter() {
+
+            public void mouseClicked(MouseEvent e){
+                System.out.println(emailTextField.getText());
+            }
+        });
+        panel.add(sendLoginButton);
+
+
+
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+
+                try {
+                    LoginForm loginForm = new LoginForm();
+                    loginForm.setVisible(true);
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+    }
+}
--- JavaProject/src/boundary/LoginView.java
+++ JavaProject/src/boundary/LoginView.java
@@ -4,18 +4,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class LoginView extends JFrame{
     private JButton loginButton;
     private JPanel panel;
     private JButton registerButton;
-    private JPanel loginpanel;
-    private JPanel registerpanel;
-    private JLabel Email;
-    private JLabel Password;
-    private JPasswordField passwordfield;
-    private JTextField emailtxt;
-    private JButton sendlogin;
+
 
 
     public LoginView() {
@@ -33,45 +29,14 @@ public class LoginView extends JFrame{
 
         JButton loginButton = new JButton("Login");
         loginButton.setBounds(10,30,90,30);
-        loginButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                JPanel loginpanel = new JPanel();
+        loginButton.addMouseListener(new MouseAdapter() {
+
+            public void mouseClicked(MouseEvent e) {
+
+                LoginForm loginForm = new LoginForm();
+                loginForm.setVisible(true);
 
 
-
-                loginpanel.setLayout(null);
-                loginpanel.setBounds(5,10,5,5);
-                setContentPane(loginpanel);
-
-                JLabel Email = new JLabel("Email");
-                Email.setBounds(10,30,90,30);
-                Email.setFont(new Font("Arial",Font.BOLD,25));
-                loginpanel.add(Email);
-
-                emailtxt = new JTextField();
-                emailtxt.setBounds(10,30,90,30);
-
-                loginpanel.add(emailtext);
-                emailtxt.setColumns(10);
-
-
-                JLabel Password = new JLabel("Password");
-                Password.setBounds(10,30,90,30);
-                Password.setFont(new Font("Arial",Font.BOLD,25));
-                loginpanel.add(Password);
-
-                passwordfield= new JPasswordField();
-                passwordfield.setBounds(10,30,90,30);
-                loginpanel.add(passwordfield);
-                passwordfield.setColumns(10);
-
-                JButton sendlogin = new JButton("Send Login");
-                sendlogin.setBounds(10,30,90,30);
-                sendlogin.addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                        System.out.println("Email:"+emailtxt.getText()+"\t\t Password"+ passwordfld.getText());
-                    }
-                });
             }
 
         });
@@ -81,12 +46,6 @@ public class LoginView extends JFrame{
         registerButton.setBounds(120,30,90,30);
         registerButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                panel.removeAll();
-                JPanel registerpanel = new JPanel();
-                registerpanel.setBounds(5,5,5,5);
-                registerpanel.setLayout(null);
-                setContentPane(registerpanel);
-
 
 
 

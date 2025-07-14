1a9b9dc66b7ea271b416f81ef8cf7668061d293a
Mario Calcagno <mario.calcagno@protonmail.com>
2025-07-14 18:17:09 +0200

    Creato PasswordManager per la gestione delle password

--- JavaProject/src/utily/PasswordManager.java
+++ JavaProject/src/utily/PasswordManager.java
@@ -1,4 +1,34 @@
 package utily;
 
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class PasswordManager {
+    private static PasswordManager instance;
+    private final Properties props;
+
+    private PasswordManager() {
+	this.props = new Properties();
+	try(FileInputStream fs =new FileInputStream("config/system.properties")){
+	    props.load(fs);
+	} catch (IOException e) {
+	    throw new RuntimeException("Failes to load system properties", e);
+	}
+    }
+
+    public static PasswordManager getIstance() {
+	if(instance == null){
+	    synchronized (PasswordManager.class) {
+		if(instance == null)
+		    instance = new PasswordManager();
+	    }
+	}
+	return instance;
+    }
+
+    public String get(String key) {
+	return props.getProperty(key);
+    }
 }
+

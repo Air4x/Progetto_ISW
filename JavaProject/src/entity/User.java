df95ff9044018d2e8f1c8c7479e1aff791a9e8cc
Mario Calcagno <mario.calcagno@protonmail.com>
2025-07-19 13:13:41 +0200

    Aggiunta documentazione in JavaDoc

--- JavaProject/src/entity/User.java
+++ JavaProject/src/entity/User.java
@@ -1,13 +1,43 @@
 package entity;
-
+/**
+ * Classe di dominio astratta, contiene i dati e le funzionalità comuni tra {@see entity.Author} e {@see entity.Organizer}
+ * @author Mario Calcagno
+ */
 public abstract class User {
+    /**
+     * Il codice identificativo dell'utente, è un UUIDv?
+     */
     private String id;
+    /**
+     * Il nome dell'utente
+     */
     private String name;
+    /**
+     * Il cognome dell'utente
+     */
     private String lastName;
+    /**
+     * L'email dell'utente
+     */
     private String email;
+    /**
+     * L'università dell'utente
+     */
     private String affiliazione;
+    /**
+     * Lo SHA256 della password dell'utente
+     */
     private String password;
 
+    /**
+     * Istanzia un utente
+     * @param affiliazione
+     * @param email
+     * @param lastName
+     * @param name
+     * @param password
+     * @param id 
+     */
     protected User(String affiliazione, String email, String lastName, String name, String password, String id) {
         this.affiliazione = affiliazione;
         this.email = email;
@@ -16,8 +46,12 @@ public abstract class User {
         this.password = password;
         this.id = id;
     }
-
-    protected User(User a) {
+    
+    /**
+     * Costruttore di copia
+     * @param user
+     */
+    protected User(User user) {
 	this.affiliazione = a.getAffiliazione();
         this.email = a.getEmail();
         this.lastName = a.getLastName();
@@ -26,37 +60,73 @@ public abstract class User {
         this.id = a.getId();
     }
 
+    /**
+     * getter per id
+     * @return l'id dell'utente
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * getter per il nome dell'utente
+     * @return il nome dell'utente
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * getter per il cognome dell'utente
+     * @return il cognome dell'utente
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * getter per l'affiliazione dell'utente
+     * @return l'affiliazione dell'utente
+     */
     public String getAffiliazione() {
         return affiliazione;
     }
 
+    /**
+     * getter per l'email dell'utente
+     * @return l'email dell'utente
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * getter per la password dell'utente
+     * @return la password dell'utente
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Cambia l'affiliazione dell'utente
+     * @param la nuova affiliazione
+     */
     public void setAffiliazione(String affiliazione) {
         this.affiliazione = affiliazione;
     }
 
+    /**
+     * Cambia l'email per l'utente
+     * @param la nuova email dell'utente
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Metodo astratto implementato da le sotto classi
+     * @return il ruolo dell'utente
+     */
     public abstract String getRole();
 }

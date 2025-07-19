4e0bd350b27842db2cdded5c57bb40f7c686a06f
Mario Calcagno <mario.calcagno@protonmail.com>
2025-07-19 13:19:38 +0200

    Aggiunta documentazione in JavaDoc in Author.java

--- JavaProject/src/entity/Author.java
+++ JavaProject/src/entity/Author.java
@@ -1,20 +1,46 @@
 package entity;
-
+/**
+ * Classe che implementa un Autore, estende {@see entity.User}
+ *
+ */
 public class Author extends User{
+    /**
+     * Il ruolo dell'autore
+     */
     final String role;
 
+    /**
+     * Costruttore di Author
+     *
+     * @param affiliazione
+     * @param email
+     * @param lastName
+     * @param name
+     * @param password
+     * @param id 
+     */
     public Author(String affiliazione, String email, String lastName,
                   String name, String password, String id) {
         super(affiliazione, email, lastName, name, password, id);
         this.role = "autore";
     }
 
-    public Author(Author a){	
-        super(a.getAffiliazione(), a.getEmail(), a.getLastName(),
-	      a.getName(), a.getPassword(), a.getId());
-	this.role = a.getRole();
+    /**
+     * Costruttore di copia
+     *
+     * @param autore 
+     */
+    public Author(Author autore){	
+        super(autore.getAffiliazione(), autore.getEmail(), autore.getLastName(),
+	      autore.getName(), autore.getPassword(), autore.getId());
+	this.role = autore.getRole();
     }	
 
+    /**
+     * Implementazione di {@see User.getRole}
+     *
+     * @return il ruole dell'autore
+     */
     public String getRole() {
         return this.role;
     }

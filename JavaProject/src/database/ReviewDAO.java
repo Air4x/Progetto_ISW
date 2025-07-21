2ebe058914ac350837a0db4cc3c2f8c8c2041598
Mario Calcagno <mario.calcagno@protonmail.com>
2025-07-21 19:16:56 +0200

    Aggiornato ReviewDAO per usare utility.ID

--- JavaProject/src/database/ReviewDAO.java
+++ JavaProject/src/database/ReviewDAO.java
@@ -5,7 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-
+import utility.ID;
 
 public class ReviewDAO {
     private Connection conn;
@@ -14,30 +14,30 @@ public class ReviewDAO {
 	conn = DBManager.getConnection();
     }
 
-    public boolean hasConflitOfInterest(String idArt, String idUser) throws SQLException {
+    public boolean hasConflitOfInterest(ID idArt, ID idUser) throws SQLException {
 	ArrayList<String> autori = new ArrayList<>();
-	String fromAutori = "SELECT id_aut FROM Autori WHERE id_art = " + idArt +";";
+	String fromAutori = "SELECT id_aut FROM Autori WHERE id_art = " + idArt.toString() +";";
 	Statement stAutori = conn.createStatement();
 	ResultSet rsAutori = stAutori.executeQuery(fromAutori);
 	while(rsAutori.next()) {
 	    autori.add(rsAutori.getString("id_aut"));
 	}
-	if (autori.contains(idUser)) {
+	if (autori.contains(idUser.toString())) {
 	    return true;
 	} else {
 	    return false;
 	}
     }
     
-    public void assignReviewer(String idArt, String idRev) throws SQLException {
-	String intoRevisori = "INSERT INTO Revisori VALUES("+idArt+","+idRev+");";
+    public void assignReviewer(ID idArt, String idRev) throws SQLException {
+	String intoRevisori = "INSERT INTO Revisori VALUES("+idArt.toString()+","+idRev.toString()+");";
 	Statement stRevisori = conn.createStatement();
 	stRevisori.executeUpdate(intoRevisori);
     }
 
-    public ArrayList<String> getReviewersByArticles(String idArt) throws SQLException {
+    public ArrayList<String> getReviewersByArticles(ID idArt) throws SQLException {
 	ArrayList<String> revisori = new ArrayList<>();
-	String fromRevisori = "SELECT id_rev FROM Revisori WHERE id_art = "+ idArt + ";";
+	String fromRevisori = "SELECT id_rev FROM Revisori WHERE id_art = "+ idArt.toString() + ";";
 	Statement stRevisori = conn.createStatement();
 	ResultSet rsRevisori = stRevisori.executeQuery(fromRevisori);
 	while(rsRevisori.next()) {

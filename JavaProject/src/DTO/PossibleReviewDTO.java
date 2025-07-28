3933be462dea63c0879c13e807c89a31dbb31e71
Giuseppe Buglione <giuseppe.buglione2003@gmail.com>
2025-07-28 13:56:15 +0200

    Aggiunto PossibleReviewDTO ShowEmailDTO RUserDTO con javadoc al package DTO e aggiunto javadoc e migliorati ShowActiveConferenceDTO e ShowArticleDTO

--- JavaProject/src/DTO/PossibleReviewDTO.java
+++ JavaProject/src/DTO/PossibleReviewDTO.java
@@ -1,5 +1,82 @@
 package DTO;
 
+import utility.ID;
+import entity.Author;
+
 public class PossibleReviewDTO {
 
+    private ID id;
+    private String name;
+    private String lastname;
+    private String affiliazione;
+    private boolean selezione;
+    
+    /**
+     * Costruttore
+     * @param id
+     * @param name
+     * @param lastname
+     * @param affiliazione
+     * @param roule
+     * @param selezione
+     */
+    public PossibleReviewDTO(ID id, String name, String lastname, String affiliazione,  boolean selezione) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        this.affiliazione = affiliazione;
+        this.selezione = selezione;
+    }
+
+    /**
+     * Costruttore
+     * @param author
+     */
+    public PossibleReviewDTO(Author author) {
+        this.id = author.getId();
+        this.name = author.getName();
+        this.lastname = author.getLastName();
+        this.affiliazione = author.getAffiliazione();
+        this.selezione = false;
+    }
+
+    /**
+     * Id del revisore
+     * @return
+     */
+    public ID getId() {
+        return id;
+    }
+
+    /**
+     * Nome del revisore
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Cognome del revisore
+     * @return
+     */
+    public String getLastname() {
+        return lastname;
+    }
+
+    /**
+     * Affiliazione del revisore
+     * @return
+     */
+    public String getAffiliazione() {
+        return affiliazione;
+    }
+
+    /**
+     * Elemento per la selezione dell'autore
+     * @return
+     */
+    public boolean getSelezione() {
+        return selezione;
+    }
 }
--- JavaProject/src/DTO/ShowActiveConferenceDTO.java
+++ JavaProject/src/DTO/ShowActiveConferenceDTO.java
@@ -10,14 +10,22 @@ public class ShowActiveConferenceDTO {
     private Date scadenza;
     private String decrizione;
 
-    
-
+    /**
+     * Costrutore
+     */
     public ShowActiveConferenceDTO() {
         this.titolo = new String();
         this.scadenza = new Date();
         this.decrizione = new String();
     }
 
+    /**
+     * Costruttore
+     * @param id
+     * @param titolo
+     * @param scadenza
+     * @param decrizione
+     */
     public ShowActiveConferenceDTO(ID id, String titolo, Date scadenza, String decrizione) {
         this.id = id;
         this.titolo = titolo;
@@ -25,18 +33,34 @@ public class ShowActiveConferenceDTO {
         this.decrizione = decrizione;
     }
 
+    /**
+     * ID della conferenzza
+     * @return
+     */
     public ID getId() {
         return id;
     }
 
+    /**
+     * Titolo  della conferenzza
+     * @return
+     */
     public String getTitolo() {
         return titolo;
     }
 
+    /**
+     * Scadenza della conferenzza
+     * @return
+     */
     public Date getScadenza() {
         return scadenza;
     }
 
+    /**
+     * Descrizione della conferenzza
+     * @return
+     */
     public String getDecrizione() {
         return decrizione;
     }

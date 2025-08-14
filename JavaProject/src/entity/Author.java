aab6f7fd22bff271bb5348263fd8f51a6076ec68
Mario Calcagno <mario.calcagno@protonmail.com>
2025-08-15 01:11:57 +0200

    Autore->Author

---
+++ JavaProject/src/entity/Author.java
@@ -0,0 +1,49 @@
+package entity;
+
+import utility.ID;
+/**
+ * Classe che implementa un Autore, estende {@see entity.User}
+ *
+ */
+public class Author extends User {
+    /**
+     * Il ruolo dell'autore
+     */
+    final String role;
+
+    /**
+     * Costruttore di Author
+     *
+     * @param affiliazione
+     * @param email
+     * @param lastName
+     * @param name
+     * @param password
+     * @param id 
+     */
+    public Author(String affiliazione, String email, String lastName,
+                  String name, String password, ID id) {
+        super(affiliazione, email, lastName, name, password, id);
+        this.role = "autore";
+    }
+
+    /**
+     * Costruttore di copia
+     *
+     * @param autore 
+     */
+    public Author(Author autore){
+        super(autore.getAffiliation(), autore.getEmail(), autore.getLastName(),
+	      autore.getName(), autore.getPassword(), autore.getId());
+	this.role = autore.getRole();
+    }	
+
+    /**
+     * Implementazione di {@see User.getRole}
+     *
+     * @return il ruole dell'autore
+     */
+    public String getRole() {
+        return this.role;
+    }
+}

42f4ab9ce1df2d20dbe568b46949c1b6d124bb0d
Mario Calcagno <mario.calcagno@protonmail.com>
2025-08-17 12:09:56 +0200

    Aggiornato create_database.sql per accomodare l'uso degli UUID

--- create_database.sql
+++ create_database.sql
@@ -4,7 +4,7 @@ CREATE DATABASE testDB;
 USE testDB;
 -- Tabella Utenti
 CREATE TABLE Utenti (
-       ID VARCHAR(5) PRIMARY KEY,
+       ID VARCHAR(36) PRIMARY KEY,
        NOME VARCHAR(100) NOT NULL,
        COGNOME VARCHAR(100) NOT NULL,
        AFFILIAZIONE VARCHAR(100) NOT NULL,
@@ -16,14 +16,14 @@ CREATE TABLE Utenti (
 
 -- Tabella Articoli
 CREATE TABLE Articoli (
-       ID VARCHAR(5) PRIMARY KEY,
+       ID VARCHAR(36) PRIMARY KEY,
        TITOLO VARCHAR(140) NOT NULL,
        ABSTRACT VARCHAR(250) NOT NULL
 );
 
 -- Tabella Conferenze
 CREATE TABLE Conferenze (
-       ID VARCHAR(5) PRIMARY KEY,
+       ID VARCHAR(36) PRIMARY KEY,
        TITOLO VARCHAR(140) NOT NULL,
        DESCRIZIONE VARCHAR(250) NOT NULL,
        SCADENZA	   DATE NOT NULL
@@ -31,8 +31,8 @@ CREATE TABLE Conferenze (
 
 -- Tabella Autori
 CREATE TABLE Autori (
-       id_aut VARCHAR(5),
-       id_art VARCHAR(5),
+       id_aut VARCHAR(36),
+       id_art VARCHAR(36),
        CONSTRAINT PK_AUTORE PRIMARY KEY(id_aut, id_art),
        CONSTRAINT FK_AUTORI_UTENTI FOREIGN KEY(id_aut) REFERENCES Utenti(ID),
        CONSTRAINT FK_AUTORI_ARTICOLI FOREIGN KEY(id_art) REFERENCES Articoli(ID)
@@ -40,8 +40,8 @@ CREATE TABLE Autori (
 
 -- Tabella Registro
 CREATE TABLE Registro (
-       id_conf VARCHAR(5),
-       id_art VARCHAR(5),
+       id_conf VARCHAR(36),
+       id_art VARCHAR(36),
        stato  VARCHAR(13),
        CONSTRAINT PK_REGISTRO PRIMARY KEY(id_conf, id_art),
        CONSTRAINT FK_REGISTRO_CONFERENZE FOREIGN KEY(id_conf) REFERENCES Conferenze(ID),
@@ -50,8 +50,8 @@ CREATE TABLE Registro (
 
 -- Tabella Revisori
 CREATE TABLE Revisori (
-       id_art VARCHAR(5),
-       id_rev VARCHAR(5),
+       id_art VARCHAR(36),
+       id_rev VARCHAR(36),
        CONSTRAINT PK_REVISORI PRIMARY KEY(id_conf, id_art, id_rev),
        CONSTRAINT FK_REVISORI_ARTICOLI FOREIGN KEY(id_art) REFERENCES Articoli(id),
        CONSTRAINT FK_REVISORI_UTENTI FOREIGN KEY (id_rev) REFERENCES Utenti(id)
@@ -59,36 +59,36 @@ CREATE TABLE Revisori (
 
 -- Inserimento Utenti
 INSERT INTO Utenti
-VALUES ('12342', 'Gianmarco', 'Rombanini', 'Seconda Università di Napoli', 'gian.rombanini@outlook.it', 'batuffolino', 'autore');
+VALUES ('9c388e06-3c9e-43bd-9327-acbffed869d3', 'Gianmarco', 'Rombanini', 'Seconda Università di Napoli', 'gian.rombanini@outlook.it', 'batuffolino', 'autore');
 INSERT INTO Utenti
-VALUES ('12354', 'Giuseppe', 'Aceto', 'Università degli Studi di Napoli Federico II', 'giuseppe.aceto@unina.it', '12345678!', 'autore');
+VALUES ('3a9e468f-ff6b-4a84-bbc0-fb3f9e9c5024', 'Giuseppe', 'Aceto', 'Università degli Studi di Napoli Federico II', 'giuseppe.aceto@unina.it', '12345678!', 'autore');
 INSERT INTO Utenti
-VALUES ('11111', 'Domenico', 'Cotroneo', 'Università degli Studi di Napoli Federico II', 'domenico.cotroneo@unina.it', 'virtualizzazione', 'organizzatore');
+VALUES ('ee719226-43d5-4bfc-bf46-3e409bbbf425', 'Domenico', 'Cotroneo', 'Università degli Studi di Napoli Federico II', 'domenico.cotroneo@unina.it', 'virtualizzazione', 'organizzatore');
 
 -- Inserimento Articoli
 INSERT INTO Articoli
-VALUES ('14141', 'Titolo di prova', 'Un abstract molto corto');
+VALUES ('7cf18f80-b41a-42f0-af41-fbb9b60303ab', 'Titolo di prova', 'Un abstract molto corto');
 INSERT INTO Articoli
-Values ('51512', 'Lonely spacecraft can navigate the stars', 'NASA`s New Horizons probe, which hurtled past Pluto in 2015, demonstrates that it can sail through interstellar space using its onboard camera');
+Values ('2e24cd58-a3d7-4057-a1b8-ce9a24669cea', 'Lonely spacecraft can navigate the stars', 'NASA`s New Horizons probe, which hurtled past Pluto in 2015, demonstrates that it can sail through interstellar space using its onboard camera');
 INSERT INTO Articoli
-VALUES ('53672', 'Evidence for a sub-jovian planet in the young TWA 7 disk', 'Using the James Webb Space Telescope Mid-Infrared Instrument, a study reports evidence for a direct detection of a cold, sub-Jupiter-mass planet in the disk of the star TWA 7');
+VALUES ('90d0f680-b4c1-416f-903c-3d2976025efb', 'Evidence for a sub-jovian planet in the young TWA 7 disk', 'Using the James Webb Space Telescope Mid-Infrared Instrument, a study reports evidence for a direct detection of a cold, sub-Jupiter-mass planet in the disk of the star TWA 7');
 
 -- Inserimento Conferenze
 INSERT INTO Conferenze
-VALUES ('51232', 'Interstellar UniNa', 'Viaggi interstellari nel mondo universitario');
+VALUES ('6279c9e1-b121-4c7a-a196-7a43b57fc16d', 'Interstellar UniNa', 'Viaggi interstellari nel mondo universitario');
 
 -- Inserimento Autori
 INSERT INTO Autori
-VALUES ('12354', '51512');
+VALUES ('9c388e06-3c9e-43bd-9327-acbffed869d3', '2e24cd58-a3d7-4057-a1b8-ce9a24669cea');
 INSERT INTO Autori
-VALUES ('12354', '14141');
+VALUES ('9c388e06-3c9e-43bd-9327-acbffed869d3', '90d0f680-b4c1-416f-903c-3d2976025efb');
 
 INSERT INTO Autori
-VALUES ('12342', '51512');
+VALUES ('ee719226-43d5-4bfc-bf46-3e409bbbf425', '90d0f680-b4c1-416f-903c-3d2976025efb');
 
 
 -- Inserimento Registro
 INSERT INTO Registro
-VALUES ('51232', '51512');
+VALUES ('6279c9e1-b121-4c7a-a196-7a43b57fc16d', '2e24cd58-a3d7-4057-a1b8-ce9a24669cea');
 INSERT INTO Registro
-VALUES ('51232', '53672');
+VALUES ('6279c9e1-b121-4c7a-a196-7a43b57fc16d', '90d0f680-b4c1-416f-903c-3d2976025efb');

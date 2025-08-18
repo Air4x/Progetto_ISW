966d861e30f170c7774782d109fc020a11f46789
Giuseppe Buglione <giuseppe.buglione2003@gmail.com>
2025-08-18 14:13:04 +0200

    Fatte correzioni dei nomi delle tabele ita->eng per compatibilita e risolti problemi generali

--- JavaProject/src/test/java/UserControllerTest.java
+++ JavaProject/src/test/java/UserControllerTest.java
@@ -1,4 +1,32 @@
 package test.java;
 
+import static org.junit.Assert.*;
+
+import DTO.RUserDTO;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+import controller.UserController;
+
+import java.sql.SQLException;
+
 public class UserControllerTest {
+
+    private UserController user_controller;
+
+    @Before
+    public void setUp() throws SQLException {
+        user_controller = new UserController();
+    }
+
+    @After
+    public void tearDown() throws SQLException {}
+
+    @Test
+    public void testRegisterUser() throws SQLException {
+        RUserDTO fake_user = user_controller.registerUser("Seconda Università di Napoli","gian.rombanini@outtlook.it","Rombanini","Gianmarco","batuffolino", "autore");
+
+    }
+
 }

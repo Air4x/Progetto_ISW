a8c990737daf9511944567926fb9e1a25c0864b2
Mario Calcagno <mario.calcagno@protonmail.com>
2025-08-18 15:56:58 +0200

    Corretti i metodi in ConferenceDAO

--- JavaProject/src/database/ConferenceDAO.java
+++ JavaProject/src/database/ConferenceDAO.java
@@ -37,11 +37,11 @@ public class ConferenceDAO {
      * conferenza ottenuta
      */
     public Conference getConferenceByID(ID id) throws SQLException {
-        String sql = "SELECT * FROM conference WHERE id = " + id;
+        String sql = "SELECT * FROM Conferenze WHERE ID = ?";
         PreparedStatement stmt = conn.prepareStatement(sql);
 	stmt.setString(1, id.toString());
         ResultSet rs = stmt.executeQuery();
-        return new Conference(rs.getDate("scadenza"), rs.getString("titolo"), rs.getString("descrizione"), new ID(rs.getString("id")));
+        return new Conference(rs.getDate("SCADENZA"), rs.getString("TITOLO"), rs.getString("DESCRIZIONE"), new ID(rs.getString("ID")));
     }
 
     /**
@@ -50,13 +50,13 @@ public class ConferenceDAO {
      * @param La conferenza da salvare
      */
     public void saveConference(Conference conf) throws SQLException {
-        String sql = "INSERT INTO conferenza(id, titolo, descrizione, scadenza) VALUES(?, ?, ?, ?);";
+        String sql = "INSERT INTO Conferenze(ID, TITOLO, DESCRIZIONE, SCADENZA) VALUES(?, ?, ?, ?);";
         PreparedStatement stmt = conn.prepareStatement(sql);
 	stmt.setString(1, conf.getId().toString());
 	stmt.setString(2, conf.getTitle());
 	stmt.setString(3, conf.getDescription());
 	stmt.setString(4, conf.getDeadline().toString());
-        int nRowsUpadated = stmt.executeUpdate();
+        int _ = stmt.executeUpdate();
     }
 
     /**
@@ -77,10 +77,10 @@ public class ConferenceDAO {
 	String queryIdAuth = "SELECT id_aut FROM AUTORI WHERE id_art = ?";
 	PreparedStatement stIdAuth = conn.prepareStatement(queryIdAuth);
 	// =======Ottenimento dati autore=====================
-	String queryAuth = "SELECT id, nome, cognome, password, affiliazione, email FROM USER WHERE id = ?";
+	String queryAuth = "SELECT ID, NOME, COGNOME, PASSWORD, AFFILIAZIONE, EMAIL FROM USER WHERE ID = ?";
 	PreparedStatement stAuth = conn.prepareStatement(queryAuth);
 	// =======Ottenimento dati articolo==================
-	String queryArt = "SELECT id, titolo, abstract FROM Articoli WHERE id = ?";
+	String queryArt = "SELECT ID, TITOLO, ABSTRACT FROM Articoli WHERE ID = ?";
 	PreparedStatement stArt = conn.prepareStatement(queryArt);
 	// =======Esecuzione queries=========================
 	while(idArt.next()){
@@ -90,14 +90,14 @@ public class ConferenceDAO {
 	    while(idAuth.next()){
 		stAuth.setString(1, idAuth.getString(1));
 		ResultSet authors = stAuth.executeQuery();
-		Author a = new Author(authors.getString("affiliazione"), authors.getString("email"),
-				      authors.getString("cognome"), authors.getString("nome"),
-				      authors.getString("password"), new ID(authors.getString("id")));
+		Author a = new Author(authors.getString("AFFILIAZIONE"), authors.getString("EMAIL"),
+				      authors.getString("COGNOME"), authors.getString("NOME"),
+				      authors.getString("PASSWORD"), new ID(authors.getString("ID")));
 		autori.add(a);
 	    }
 	    stArt.setString(1, idArt.getString("id_art"));
 	    ResultSet article = stArt.executeQuery(queryArt + idArt.getString("id_art"));
-	    Article articolo = new Article(new ID(idArt.getString("id_art")),article.getString("abstract"), autori, article.getString("titolo"));
+	    Article articolo = new Article(new ID(idArt.getString("id_art")),article.getString("ABSTRACT"), autori, article.getString("TITOLO"));
 	    articoli.add(articolo);
 	}
 	return articoli;
@@ -110,7 +110,7 @@ public class ConferenceDAO {
      */
     public ArrayList<Conference> getAllConference() throws  SQLException{
 	ArrayList<Conference> conferenze = new ArrayList<>();
-	String query  ="SELECT * FROM conferenza";
+	String query  ="SELECT * FROM Conferenze";
 	PreparedStatement st = conn.prepareStatement(query);
 	ResultSet rs = st.executeQuery();
 	while(rs.next()){

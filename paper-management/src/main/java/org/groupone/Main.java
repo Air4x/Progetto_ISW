6d6a3ea19a20623bfb6739775b6158964702b517
Giuseppe Buglione <giuseppe.buglione2003@gmail.com>
2025-09-01 10:09:10 +0200

    Aggiunto in Main.java un timer per che va eseguire in maniera periodica org.groupone.controller.NotificationController.invioNotifiche()

--- paper-management/src/main/java/org/groupone/Main.java
+++ paper-management/src/main/java/org/groupone/Main.java
@@ -1,16 +1,32 @@
 package org.groupone;
 
+import java.sql.SQLException;
+import java.util.Timer;
+
+import org.groupone.controller.NotificationController;
+
+
 public class Main {
     public static void main(String[] args) {
-	Thread guiThread = new Thread(() -> {
-		System.out.println("Gui thread\n");
-	});
-	guiThread.start();
+		Timer new_timer = new Timer();
+		long periodInMillis = 3 * 24 * 60 * 60 * 1000; // calcolo di 3 giorni in millisecondi
+		
+		Thread guiThread = new Thread(() -> {
+			System.out.println("Gui thread\n");
+		});
+		guiThread.start();
 
-	Thread notificationThread = new Thread(() -> {
-		System.out.println("Email notifcations thread\n");
-	});
-	notificationThread.start();
-    }
+		Thread notificationThread = new Thread(() -> {
+			System.out.println("Email notifications thread\n");
+			try {
+				NotificationController notification_task = new NotificationController();
+				new_timer.schedule(notification_task, 0, periodInMillis);
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("Errore nell'inizializzazione del NotificationController");
+			}
+		});
+		notificationThread.start();
+    	}
 
 }

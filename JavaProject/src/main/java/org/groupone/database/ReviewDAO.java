c7ab856cc73cf89c49a2be353a794e34089b7e0a
Mario Calcagno <mario.calcagno@protonmail.com>
2025-10-06 17:09:06 +0200

    feat!: Implementati i metodi per la classe Review
    Segnati i vecchi metodi come deprecati

--- JavaProject/src/main/java/org/groupone/database/ReviewDAO.java
+++ JavaProject/src/main/java/org/groupone/database/ReviewDAO.java
@@ -5,7 +5,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 import org.groupone.utility.ID;
+import org.groupone.entity.Review;
+import org.groupone.entity.Article;
+import org.groupone.entity.Author;
 
 
 /**
@@ -38,7 +42,7 @@ public class ReviewDAO {
      */
     public boolean hasConflitOfInterest(ID idArt, ID idUser) throws SQLException {
 	ArrayList<String> autori = new ArrayList<>();
-	String fromAutori = "SELECT id_aut FROM Autori WHERE id_art = ?;";
+	String fromAutori = "SELECT ID_UTENTE FROM Autori WHERE ID_ARTICOLO = ?;";
 	PreparedStatement stAutori = conn.prepareStatement(fromAutori);
 	stAutori.setString(1, idArt.toString());
 	ResultSet rsAutori = stAutori.executeQuery();
@@ -52,12 +56,90 @@ public class ReviewDAO {
 	}
     }
 
+
+    /**
+     * Permette di salvare una revisione nel database
+     *
+     * @param review, la revisione da salvare
+     */
+
+    public void saveReview(Review review) throws SQLException {
+	String intoRevisioni = "INSERT INTO Revisioni VALUES (?, ?, ?, ?, ?);";
+	PreparedStatement stRevisioni = conn.prepareStatement(intoRevisioni);
+	stRevisioni.setString(1, review.getId().toString());
+	stRevisioni.setString(2, review.getReviewer().getId().toString());
+	stRevisioni.setString(3, review.getArticle().getId().toString());
+	stRevisioni.setInt(4, review.getScore());
+	stRevisioni.setString(5, review.getResult());
+	stRevisioni.executeUpdate();
+    }
+
+    /**
+     * Permette di ottenere la lista di tutte le revisioni assegnare
+     * ad un dato autore
+     *
+     * @param reviewer, l'id del revisore
+     */
+    public List<Review> getAllReviewByReviewer(ID reviewer) throws SQLException {
+	ArrayList<Review> revs = new ArrayList<>();
+	UserDAO userdao = new UserDAO();
+	ArticleDAO articledao = new ArticleDAO();
+	String fromRevisioni = "SELECT ID, ID_REVISORE, ID_ARTICOLO, PUNTEGGIO, ESITO FROM Revisioni WHERE ID_REVISORE = ?;";
+	PreparedStatement stRevisioni = conn.prepareStatement(fromRevisioni);
+	stRevisioni.setString(1, reviewer.toString());
+	ResultSet rs = stRevisioni.executeQuery();
+	while(rs.next()){
+	    Article art = articledao.getArticleByID(new ID(rs.getString("ID_ARTICOLO")));
+	    Author aut = (Author) userdao.getUserByID(new ID(rs.getString("ID_REVISORE")));
+	    revs.add(new Review(new ID(rs.getString("ID")), aut, art, rs.getInt("PUNTEGGIO"), rs.getString("ESITO")));
+	}
+	return revs;
+     }
+    
+    /**
+     * Permette di ottenere la lista di tutte le revisioni di un dato
+     * articolo
+     *
+     * @param articolo, l'id dell'articolo
+     */
+    public List<Review> getAllReviewByArticle(ID article) throws SQLException {
+	ArrayList<Review> revs = new ArrayList<>();
+	UserDAO userdao = new UserDAO();
+	ArticleDAO articledao = new ArticleDAO();
+	String fromRevisioni = "SELECT ID, ID_REVISORE, ID_ARTICOLO, PUNTEGGIO, ESITO FROM Revisioni WHERE ID_ARTICOLO = ?;";
+	PreparedStatement stRevisioni = conn.prepareStatement(fromRevisioni);
+	stRevisioni.setString(1, article.toString());
+	ResultSet rs = stRevisioni.executeQuery();
+	while(rs.next()){
+	    Article art = articledao.getArticleByID(new ID(rs.getString("ID_ARTICOLO")));
+	    Author aut = (Author) userdao.getUserByID(new ID(rs.getString("ID_REVISORE")));
+	    revs.add(new Review(new ID(rs.getString("ID")), aut, art, rs.getInt("PUNTEGGIO"), rs.getString("ESITO")));
+	}
+	return revs;
+     }
+
+    /**
+     * Permette di aggiornare il punteggio e l'esito di una revisione.
+     *
+     * @param rev, la Review da aggiornare
+     */
+    public void updateReview(Review rev) throws SQLException{
+	String st = "UPDATE Revisioni SET ESITO = ?, PUNTEGGIO = ? WHERE ID = ?;";
+	PreparedStatement pst = conn.prepareStatement(st);
+	pst.setString(1, rev.getResult());
+	pst.setInt(2, rev.getScore());
+	pst.setString(3, rev.getId().toString());
+	pst.executeUpdate();
+    }
+    
+    
     /**
      * Permette di assegnare un autore come revisore ad un articolo
      *
      * @param L'id dell'articolo
      * @param L'id del revisore
      */
+    @Deprecated
     public void assignReviewer(ID idArt, ID idRev) throws SQLException {
 	String intoRevisori = "INSERT INTO Revisori VALUES(?, ?);";
 	PreparedStatement stRevisori = conn.prepareStatement(intoRevisori);
@@ -72,6 +154,7 @@ public class ReviewDAO {
      * @param L'id dell'articolo
      * @return La lista dei revisori assegnati
      */
+    @Deprecated
     public ArrayList<String> getReviewersByArticle(ID idArt) throws SQLException {
 	ArrayList<String> revisori = new ArrayList<>();
 	String fromRevisori = "SELECT id_rev FROM Revisori WHERE id_art = ?;";
@@ -83,13 +166,14 @@ public class ReviewDAO {
 	}
 	return revisori;
     }
-
+    
     /**
      * Permette di aggiornare lo stato di un articolo
      *
      * @param L'id dell'articolo
      * @param Il nuovo stato
      */
+    @Deprecated
     public void updateArticleStatus(ID id, String status) throws SQLException{
 	String intoRegistro = "UPDATE Registro SET STATUS = ? WHERE id_art = ?;";
 	PreparedStatement st = conn.prepareStatement(intoRegistro);

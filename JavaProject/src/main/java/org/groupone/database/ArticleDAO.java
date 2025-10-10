78e88700ea8ae5ea888ff7bc89445d1502c80c72
Mario Calcagno <mario.calcagno@protonmail.com>
2025-10-10 18:41:11 +0200

    fix: Corretta query SQL in updateTitle

--- JavaProject/src/main/java/org/groupone/database/ArticleDAO.java
+++ JavaProject/src/main/java/org/groupone/database/ArticleDAO.java
@@ -141,7 +141,7 @@ public class ArticleDAO {
      * @param newTitle, il nuovo titolo dell'articolo
      */
     public void updateTitle(ID artId, String newTitle) throws SQLException {
-	String updateQuery = "UPDATE Articoli SET Articoli.TITLE=? WHERE Articoli.ID=?;";
+	String updateQuery = "UPDATE Articoli SET Articoli.TITOLO=? WHERE Articoli.ID=?;";
 	PreparedStatement updateArticoli = conn.prepareStatement(updateQuery);
 	updateArticoli.setString(1, artId.toString());
 	updateArticoli.setString(2, newTitle);

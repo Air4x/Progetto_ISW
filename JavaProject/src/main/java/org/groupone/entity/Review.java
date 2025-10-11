d5db40dd9b0e15abad7f28b3d9f33b514d08fe5b
Mario Calcagno <mario.calcagno@protonmail.com>
2025-10-11 21:45:10 +0200

    doc: Aggiunta documentazione javadoc per Review

--- JavaProject/src/main/java/org/groupone/entity/Review.java
+++ JavaProject/src/main/java/org/groupone/entity/Review.java
@@ -2,13 +2,43 @@ package org.groupone.entity;
 
 import org.groupone.utility.ID;
 
+
+/**
+ * Classe che rappresenta una revisione
+ *
+ */
 public class Review {
+    /**
+     * L'id della revisione
+     *
+     */
     private ID id;
+    /**
+     * L'autore a cui è stata assegnara la revisione
+     *
+     */
     private Author reviewer;
+    /**
+     * L'articolo di cui si fa la revisione
+     *
+     */
     private Article article;
+    /**
+     * Il punteggio della revisione
+     *
+     */
     private int score;
+    /**
+     * L'esito della revisione
+     *
+     */
     private String result;
 
+    /**
+     * Costruttore di Review, utilizza valori di default
+     *
+     * @param reviewer
+     */
     public Review(Author reviewer, Article article) {
 	this.id = ID.generate();
 	this.reviewer = reviewer;
@@ -16,7 +46,16 @@ public class Review {
 	this.score = 0;
 	this.result = "inattesa";
     }
-
+    
+    /**
+     * Costruttore di Review, senza valori di default
+     *
+     * @param id
+     * @param reviewer
+     * @param article
+     * @param score
+     * @param esito
+     */
     public Review(ID id, Author reviewer, Article article, int score, String esito){
 	this.id = id;
 	this.reviewer = reviewer;
